6f9ee88bf75e4a7f9bc3948ce61e29c96bb96b1a
Nilesh <nileshsatishkulkarni@gmail.com>
2014-02-05 16:34:25 +0530

    astar started

---
+++ lab5/node.cpp
@@ -0,0 +1,19 @@
+#include<list>
+
+using namespace std;
+
+class Node{
+    
+    list<Node*> neighbours
+    bool visted;  
+
+    
+}
+
+
+class Graph{
+    list<Node*> allNodes;
+   
+    void getShortestPath(int start, int end);
+
+}
