3ddcb76cc105bc51f99b6a7755db30b61b5882ad
Nilesh <nileshsatishkulkarni@gmail.com>
2014-02-12 03:14:54 +0530

    trying to remove seg fault

--- lab5/node.h
+++ lab5/node.h
@@ -1,11 +1,13 @@
 #ifndef NODE_H
 #define NODE_H
 #include<iostream>
+#include<vector>
 #include<list>
 
 using namespace std;
 class Node;
 typedef Node* NodePtr;
+typedef vector<NodePtr> nodeVector;
 
 class Node{
     public:
@@ -17,5 +19,13 @@ class Node{
     int dS;
     int g_score ;
     int f_score ;
+    Node(int i){
+        id =i;
+        g_score = 1000 ;
+        f_score = 1000 ;
+    }
+    void addNeighbour(NodePtr n){
+        neighbours.push_back(n);
+    }
 };
 #endif
