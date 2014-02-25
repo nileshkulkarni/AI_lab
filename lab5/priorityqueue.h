f27de37ea22469b9d3680596f9c8368c523540b1
sushant <hiraysushant@gmail.com>
2014-02-26 01:51:48 +0530

    priority queue implemented

--- lab5/priorityqueue.h
+++ lab5/priorityqueue.h
@@ -1,26 +1,21 @@
 #ifndef PRIORITY_H
 #define PRIORITY_H
 
-#include<stdio.h>
-#include<iostream>
-#include<vector>
-#include<list>
-#include "header.h"
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cassert>
 using namespace std;
 
 class PriorityQueue
 {
+public:
     vector<int> pq_keys;
     void shiftRight(int low, int high);
     void shiftLeft(int low, int high);
     void buildHeap();
-public:
-    PriorityQueue(){}
-    PriorityQueue(vector<int>& items)
-    {
-        pq_keys = items;
-        buildHeap();
-    }
+    PriorityQueue();
+    PriorityQueue(vector<int>& items);
     /*Insert a new item into the priority queue*/
     void enqueue(int item);
     /*Get the maximum element from the priority queue*/
