d6928324fbf418793e5cf721529f7d0d6ef896c3
Nilesh <nileshsatishkulkarni@gmail.com>
2014-04-14 21:38:53 -0800

    added functions to NeuralNetwork header

--- lab3/NeuralNetwork.h
+++ lab3/NeuralNetwork.h
@@ -2,4 +2,21 @@
 #include<stdio.h>
 #include<list>
 #include<vector>
+#include "network_layer.h"
+
 using namespace std;
+
+
+class NeuralNetwork{
+    private:    
+        vector<NetworkLayer>layers;
+        int noOfLayers; 
+        void _init();
+        
+    public:
+        void NeuralNetwork();
+        void addTrainData(vector<float> input, vector<float> outputs); 
+        void addHiddenLayer(); 
+        void addAllTrainData(vector< vector <float>> input,vector< vector<float> > output); 
+        vector<float> getOuput(vector<float> input);    
+}
