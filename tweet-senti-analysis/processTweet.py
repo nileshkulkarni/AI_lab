e97dfce7bfe2abde8cf2685569b6d41cbf570539
sushant <hiraysushant@gmail.com>
2014-04-14 21:38:53 -0800

    Final version 530 words FTW!

--- tweet-senti-analysis/processTweet.py
+++ tweet-senti-analysis/processTweet.py
@@ -76,8 +76,8 @@ def getFeatureVector(tweet,featureVector,stopWords):
         if(w.lower() in stopWords or val is None):
             continue
         else:
-            if(w.lower() not in featureVector):
-                featureVector.append(w.lower())
+            #if(w.lower() not in featureVector):
+            featureVector.append(w.lower())
     return featureVector
 #end
 
@@ -95,17 +95,72 @@ def getAllFeatureWords(filename,featureVector):
 #end
 
 #start printFeature vector
-def printFeature(featureVector):
-    fp = open("TweetsCorpus/featureVector.dump",'w')
+def getFreq(featureVector):
+    worddict={}
     d=enchant.Dict()
     for word in featureVector:
-        if(len(word)>=3 and d.check(word)):
-            fp.write(word)
-            fp.write('\n')
-    fp.close()
-    return
+        if(len(word)>=3):
+            if(word.endswith("es") and d.check(word[:-2])):
+                if(word[:-2] in worddict):
+                    temp = word[:-2]
+                    worddict[temp] = worddict.get(temp)+1
+                else:
+                    worddict[word[:-2]] = 1
+            elif(word.endswith("s") and d.check(word[:-1])):
+                if(word[:-1] in worddict):
+                    temp = word[:-1]
+                    worddict[temp] = worddict.get(temp)+1
+                else:
+                    worddict[word[:-1]] = 1
+            elif(word.endswith("ed") and d.check(word[:-2])):
+                if(word[:-2] in worddict):
+                    temp = word[:-2]
+                    worddict[temp] = worddict.get(temp)+1
+                else:
+                    worddict[word[:-2]] = 1
+            elif(word.endswith("ing") and d.check(word[:-3])):
+                if(word[:-3] in worddict):
+                    temp = word[:-3]
+                    worddict[temp] = worddict.get(temp)+1
+                else:
+                    worddict[word[:-3]] = 1
+            elif(word.endswith("ied") and d.check(word[:-3])):
+                if(word[:-3] in worddict):
+                    temp = word[:-3]
+                    worddict[temp] = worddict.get(temp)+1
+                else:
+                    worddict[word[:-3]] = 1
+            else:
+                if(word in worddict):
+                    worddict[word] = worddict.get(word)+1
+                else:
+                    worddict[word] = 1
+    return worddict
 #end
 
+#remove words occuring once
+def finalVector(worddict):
+    finallist = []
+    for k,v in worddict.iteritems():
+        if(v>1):
+            finallist.append(k)
+    fp = open("TweetsCorpus/sentiwords.txt",'r')
+    line  = fp.readline()
+
+    while line:
+        finallist.append(line.strip("\n"))
+        line = fp.readline()
+
+    fp.close()
+
+    fp = open("TweetsCorpus/featurewords.dump",'w')
+    
+    for word in finallist:
+        fp.write(word)
+        fp.write("\n")
+
+    fp.close()
+    return finallist
 
 #start extract_features
 def extract_features(tweet,featureVector,senti):
@@ -114,9 +169,22 @@ def extract_features(tweet,featureVector,senti):
     tweet.strip('\'"?,.!')
     words = tweet.split()
     words_up = []
-    for w in words:
-        w = w.strip('\'"?,.!')
-        words_up.append(w)
+    d=enchant.Dict()
+    for word in words:
+        word = word.strip()
+        word = word.strip('\'"?,.!') 
+        if(word.endswith("es") and d.check(word[:-2])):
+            words_up.append(word[:-2])
+        elif(word.endswith("s") and d.check(word[:-1])):
+            words_up.append(word[:-1])
+        elif(word.endswith("ed") and d.check(word[:-2])):
+            words_up.append(word[:-2])
+        elif(word.endswith("ing") and d.check(word[:-3])):
+            words_up.append(word[:-3])
+        elif(word.endswith("ied") and d.check(word[:-3])):
+            words_up.append(word[:-3])
+        else:
+            words_up.append(word)
     extractf = []
     extractf.append(senti) 
     for w in featureVector:
@@ -133,6 +201,7 @@ def extract_features(tweet,featureVector,senti):
 def printFeatureVector(allWords):
     featureVector = []
     fp=open("TweetsCorpus/processed_positive",'r')
+    d=enchant.Dict()
     line = fp.readline()
     while line:
         featureVector.append(extract_features(line,allWords,1))
