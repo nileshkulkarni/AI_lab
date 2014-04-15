edc7feaf795e8cb7e1531f6f8788df15d0839767
Sushant <hiraysushant@gmail.com>
2014-04-14 21:38:53 -0800

    Added basic feature extraction

--- tweet-senti-analysis/processTweet.py
+++ tweet-senti-analysis/processTweet.py
@@ -22,7 +22,7 @@ def processTweet(tweet):
 def processFile(filename):
     #Read the tweets one by one and process it
     read = 'TweetsCorpus/twitter_'+filename
-    write='TweetsCorpus/processed_'+filename
+    write= 'TweetsCorpus/processed_'+filename
     fr = open(read, 'r')
     fw = open(write,'a')
     line = fr.readline()
@@ -43,3 +43,59 @@ def processFile(filename):
 
 
             
+#start getStopWordList
+def getStopWordList(stopWordListFileName):
+    #read the stopwords file and build a list
+    stopWords = []
+    stopWords.append('AT_USER')
+    stopWords.append('URL')
+
+    fp = open(stopWordListFileName, 'r')
+    line = fp.readline()
+    while line:
+        word = line.strip()
+        stopWords.append(word)
+        line = fp.readline()
+    fp.close()
+    return stopWords
+#end
+
+
+#start getfeatureVector from tweets
+def getFeatureVector(tweet,featureVector,stopWords):
+    #split tweet into words
+    words = tweet.split()
+    for w in words[1:]:
+        #strip punctuation
+        w = w.strip('\'"?,.!')
+        #check if the word stats with an alphabet
+        val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", w)
+        #ignore if it is a stop word
+        if(w.lower() in stopWords or val is None):
+            continue
+        else:
+            if(w.lower() not in featureVector):
+                featureVector.append(w.lower())
+    return featureVector
+#end
+
+#start getAllFeatureWords from preprocessed tweets
+def getAllFeatureWords(filename,featureVector):
+    filename = 'TweetsCorpus/processed_'+filename
+    stopWords = getStopWordList("TweetsCorpus/stopword.txt")
+    fp = open(filename,'r')
+    line = fp.readline()
+    while line:
+        getFeatureVector(line,featureVector,stopWords)
+        line = fp.readline()
+    return
+#end
+
+#start printFeature vector
+def printFeature(featureVector):
+    fp = open("TweetsCorpus/featureVector.dump",'w')
+    for word in featureVector:
+        fp.write(word)
+        fp.write('\n')
+    return
+#end
