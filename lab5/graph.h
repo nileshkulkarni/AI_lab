0be15a018bd4a8bf5ea967714744de52e2936897
Nilesh <nileshsatishkulkarni@gmail.com>
2014-04-14 21:38:53 -0800

    some issue with nbr 6 and 3

--- lab5/graph.h
+++ lab5/graph.h
@@ -2,12 +2,15 @@
 #define GRAPH_H
 #include<list>
 #include "node.h"
+#include <stdio.h>
 using namespace std;
 
 class Graph{
     public:
     Graph(nodeVector l);
+    Graph(vector< vector<int> >);
     vector<NodePtr> allNodes;
     NodePtr operator[](int i);
+    void print();
 };
 #endif
